eb67bdbd2816534a26ebe5d60d526388390a6387
kauanmb123 <146395124+kauanmb123@users.noreply.github.com>
2024-10-22 11:03:47 -0300

    Add files via upload

---
+++ bkpCart-oAlura-main/Cartão3Trimestre/perguntas.js
@@ -0,0 +1,28 @@
+criaCARTAO(
+//categoria,
+'Programação'
+//pergunta,
+'O que é JavasScript'
+//resposta,
+'JavasScript é uma linguagem de programação'
+
+    )
+    criaCARTAO(
+        //categoria,
+        'Programação'
+        //pergunta,
+        'O que é JavasScript'
+        //resposta,
+        'JavasScript é uma linguagem de programação'
+        
+            )
+            criaCARTAO(
+                //categoria,
+                'Programação'
+                //pergunta,
+                'O que é JavasScript'
+                //resposta,
+                'JavasScript é uma linguagem de programação'
+                
+                    )
+                        
